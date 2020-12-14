f978888da4bc861b8a0ed68658b576a03efa6398
Bennett Forkner <bffork@gmail.com>
2020-12-14 00:10:16 -0500

    negligible

--- src/Menu.java
+++ src/Menu.java
@@ -1,8 +1,12 @@
 
 public class Menu {
 	public static void main(String[] args) {
+		if (System.console() == null) System.setProperty("jansi.passthrough", "true");
+		System.out.println(System.getProperty("os.name"));
 		if (System.getProperty("os.name").substring(0,6).compareToIgnoreCase("mac os") == 0) {
 			PRINTER.initMAC();
+		} else if (System.getProperty("os.name").substring(0,10).compareToIgnoreCase("windows 10") == 0) {
+			PRINTER.initWIN10();
 		}
 		CONTROLLER.GAME = new Game();
 		CONTROLLER.GAME.init();
--- src/PRINTER.java
+++ src/PRINTER.java
@@ -22,16 +22,8 @@ public class PRINTER {
 		WHITE = "\u001B[37m";
 	}
 	
-	public static void initWIN() {
-		RESET = "\u001B[0m";
-		BLACK = "\u001B[30m";
-		RED = "\u001B[31m";
-		GREEN = "\u001B[32m";
-		YELLOW = "\u001B[33m";
-		BLUE = "\u001B[34m";
-		PURPLE = "\u001B[35m";
-		CYAN = "\u001B[36m";
-		WHITE = "\u001B[37m";
+	public static void initWIN10() {
+		
 	}
 	
 	public static void prompt(String in) {
